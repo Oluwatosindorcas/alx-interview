56240d770a4ab6b8fa7ff670c1ca689d1aea4820
Oluwatosindorcas <oluwatosindorcas68@gmail.com>
2023-07-03 02:34:16 +0100

    Pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,24 @@
+#!/usr/bin/python3
+"""Pascal Triangle"""
+
+
 def pascal_triangle(n):
+    """fucntion to get the numbers of pascal triangle"""
     if n <= 0:
         return []
+    pascal_triangle = [0] * n
 
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
+    for i in range(n):
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
 
-    return triangle
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
