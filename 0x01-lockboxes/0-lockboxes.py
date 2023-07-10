afb1df3a9338cca45a2c64565ee091abf3dbfb55
Oluwatosindorcas <oluwatosindorcas68@gmail.com>
2023-07-10 01:30:53 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
+""" Defines a function that checks if boxes can be unlocked"""
+
+
 def canUnlockAll(boxes):
     """
-    Function that checks if available
-    keys can ulock available boxes
+    Checks if all boxes can be unlocked by iterating through
+    keys stored while visiting boxes.
+
+    Args:
+        boxes (list[list[int]]): A list of lists representing
+        the boxes and their corresponding keys.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
     """
-    # return false if first box is empty
-    if len(boxes[0]) == 0:
-        return False
-    for box in boxes:
-        # skip first box
-        if box == boxes[0]:
+    key_store = [0]
+    index = key = count = 0
+    while index < len(key_store):
+        key = key_store[index]
+        if key >= len(boxes):
+            index += 1
             continue
-        # return false if box is empty
-        if len(box) == 0:
-            return False
-        # return false if box is not reachable
-        if box not in boxes[0]:
-            return False
-    return True
+        for k in boxes[key]:
+            if k not in key_store:
+                key_store.append(k)
+        index += 1
+        count += 1
+    return count == len(boxes)
